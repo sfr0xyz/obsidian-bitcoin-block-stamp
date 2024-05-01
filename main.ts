df52aa080aff38211c684ded73314bbb6ec36dd6
sefiro <sefiro@sfr0.xyz>
2024-05-01 08:50:22 +0200

    fix: change indentation tabs to spaces

--- main.ts
+++ main.ts
@@ -6,108 +6,119 @@ import { insertAtCursor, replacePlaceholders } from '@utils/utils';
 import { Replacements } from '@utils/types';
 
 export default class BbsPlugin extends Plugin {
-	settings: BbsPluginSettings;
+  settings: BbsPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+    await this.loadSettings();
 
-		this.addRibbonIcon('bitcoin', 'Insert custom Bitcoin block stamp', () => {
-			new CustomStampModal(this.app, this).open();
-		});
+    this.addRibbonIcon('bitcoin', 'Insert custom Bitcoin block stamp', () => {
+      new CustomStampModal(this.app, this).open();
+    });
 
-		this.addCommand({
-			id: 'insert-current-block-height',
-			name: 'Insert current block height',
-			editorCallback: async (editor: Editor) => {
-				try {
-					const blockHeight: string = await new Stamp().blockHeight(this.settings.formats.blockHeight, this.settings.blockExplorer);
-					insertAtCursor(blockHeight, editor);
-				} catch (error) {
-					console.error(error);
-					new Notice('An error occured', error.message);
-				}
-			}
-		});
+    this.addCommand({
+      id: 'insert-current-block-height',
+      name: 'Insert current block height',
+      editorCallback: async (editor: Editor) => {
+        try {
+          const blockHeight: string = await new Stamp().blockHeight(this.settings.formats.blockHeight, this.settings.blockExplorer);
+          insertAtCursor(blockHeight, editor);
+        } catch (error) {
+          console.error(error);
+          new Notice('🛑 An error ocurred – Couldn\'t insert stamp', 10000);
+        }
+      }
+    });
 
-		this.addCommand({
-			id: 'insert-current-moscow-time',
-			name: 'Insert current Moscow time',
-			editorCallback: async (editor: Editor) => {
-				try {
-					const moscowTime: string = await new Stamp().moscowTime(this.settings.formats.moscowTime);
-					insertAtCursor(moscowTime, editor);
-				} catch (error) {
-					console.error(error);
-					new Notice('An error occured', error.message);
-				}	
-			}
-		});
+    this.addCommand({
+      id: 'insert-current-moscow-time',
+      name: 'Insert current Moscow time',
+      editorCallback: async (editor: Editor) => {
+        try {
+          const moscowTime: string = await new Stamp().moscowTime(this.settings.formats.moscowTime);
+          insertAtCursor(moscowTime, editor);
+        } catch (error) {
+          console.error(error);
+          new Notice('🛑 An error ocurred – Couldn\'t insert stamp', 10000);
+        }	
+      }
+    });
 
-		this.addCommand({
-			id: 'insert-current-moscow-time-at-block-height',
-			name: 'Insert current Moscow time @ block height',
-			editorCallback: async (editor: Editor) => {
-				try{
-					const moscowTimeAtBlockHeight: string = await new Stamp().moscowTimeAtBlockHeight(this.settings.formats.moscowTime, this.settings.formats.blockHeight, this.settings.blockExplorer);
-					insertAtCursor(moscowTimeAtBlockHeight, editor);
-				} catch (error) {
-					console.error(error);
-					new Notice('An error occured', error.message);
-				}
-			}
-		});
+    this.addCommand({
+      id: 'insert-current-moscow-time-at-block-height',
+      name: 'Insert current Moscow time @ block height',
+      editorCallback: async (editor: Editor) => {
+        try{
+          const moscowTimeAtBlockHeight: string = await new Stamp().moscowTimeAtBlockHeight(this.settings.formats.moscowTime, this.settings.formats.blockHeight, this.settings.blockExplorer);
+          insertAtCursor(moscowTimeAtBlockHeight, editor);
+        } catch (error) {
+          console.error(error);
+          new Notice('🛑 An error ocurred – Couldn\'t insert stamp', 10000);
+        }
+      }
+    });
 
-		this.addCommand({
-			id: 'insert-historical-block-stamp',
-			name: 'Insert custom block stamp',
-			editorCallback: () => {
-				new CustomStampModal(this.app, this).open();
-			}
-		});
-		
-		this.addCommand({
-			id: 'replace-stamp-placeholders',
-			name: 'Replace stamp placeholders',
-			callback: () => {
-				console.log('replace-command')
-				const activeFile = this.app.workspace.getActiveFile();
-				console.log('active file', activeFile)
-				this.replaceStampPlaceholders(activeFile as TFile);
-			}
-		});
+    this.addCommand({
+      id: 'insert-historical-block-stamp',
+      name: 'Insert custom block stamp',
+      editorCallback: () => {
+        try {
+          new CustomStampModal(this.app, this).open();
+        } catch (error) {
+          console.error(error);
+          new Notice('🛑 An error ocurred – Couldn\'t open custom block stamp modal', 10000);
+        }
+      }
+    });
+    
+    this.addCommand({
+      id: 'replace-stamp-placeholders',
+      name: 'Replace stamp placeholders',
+      callback: () => {
+        try {
+          const activeFile = this.app.workspace.getActiveFile();
+          this.replaceStampPlaceholders(activeFile as TFile);
+        } catch (error) {
+          console.error(error);
+          new Notice('🛑 An error ocurred – Couldn\'t replace stamp placeholders', 10000);
+        }
+      }
+    });
 
-		this.addSettingTab(new BbsSettingTab(this.app, this));
+    this.addSettingTab(new BbsSettingTab(this.app, this));
 
-		this.app.workspace.onLayoutReady(() => {
-			console.log('on layout ready')
-			this.app.vault.on('create', (file: TFile) => {
-				console.log('on create')
-				this.replaceStampPlaceholders(file);
-			})
-		})
-	}
+    this.app.workspace.onLayoutReady(() => {
+      this.app.vault.on('create', (file: TFile) => {
+        try {
+          this.replaceStampPlaceholders(file);
+        } catch (error) {
+          console.error(error);
+          new Notice('🛑 An error ocurred – Couldn\'t replace stamp placeholders', 10000);
+        }
+      })
+    })
+  }
 
-	onunload() {
+  onunload() {
 
-	}
+  }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 
-	async replaceStampPlaceholders (file: TFile) {
-		let replacements: Replacements = {};
-		const stamp = new Stamp();
-		replacements = {
-			[this.settings.placeholders.blockHeight]: await stamp.blockHeight(this.settings.formats.blockHeight, this.settings.blockExplorer),
-			[this.settings.placeholders.blockHeight]: await stamp.moscowTime(this.settings.formats.moscowTime),
-			[this.settings.placeholders.blockHeight]: await stamp.moscowTimeAtBlockHeight(this.settings.formats.moscowTime, this.settings.formats.blockHeight, this.settings.blockExplorer)
-		}
+  async replaceStampPlaceholders (file: TFile) {
+    let replacements: Replacements = {};
+    const stamp = new Stamp();
+    replacements = {
+      [this.settings.placeholders.blockHeight]: await stamp.blockHeight(this.settings.formats.blockHeight, this.settings.blockExplorer),
+      [this.settings.placeholders.blockHeight]: await stamp.moscowTime(this.settings.formats.moscowTime),
+      [this.settings.placeholders.blockHeight]: await stamp.moscowTimeAtBlockHeight(this.settings.formats.moscowTime, this.settings.formats.blockHeight, this.settings.blockExplorer)
+    }
 
-		replacePlaceholders(this.app.vault, file, replacements);
-	}
+    replacePlaceholders(this.app.vault, file, replacements);
+  }
 }
--- src/settings.ts
+++ src/settings.ts
@@ -3,130 +3,130 @@ import BbsPlugin from 'main';
 import { App, PluginSettingTab, Setting } from 'obsidian';
 
 export interface StampFormats {
-	blockHeight: BlockHeightFormat,
-	moscowTime: MoscowTimeFormat
+  blockHeight: BlockHeightFormat,
+  moscowTime: MoscowTimeFormat
 }
 
 export interface StampPlaceholders {
-	blockHeight: string,
-	moscowTime: string,
-	moscowTimeAtBlockHeight: string
+  blockHeight: string,
+  moscowTime: string,
+  moscowTimeAtBlockHeight: string
 }
 
 export interface BbsPluginSettings {
-	blockExplorer: BlockExplorer
-	formats: StampFormats
-	placeholders: StampPlaceholders
+  blockExplorer: BlockExplorer
+  formats: StampFormats
+  placeholders: StampPlaceholders
 }
 
 export const DEFAULT_SETTINGS: BbsPluginSettings = {
-	blockExplorer: '',
-	formats: {
-		blockHeight: 'plain',
-		moscowTime: 'plain'
-	},
-	placeholders: {
-		blockHeight: '{{blockheight}}',
-		moscowTime: '{{moscowtime}}',
-		moscowTimeAtBlockHeight: '{{moscowtime@blockheight}}'
-	}
+  blockExplorer: '',
+  formats: {
+    blockHeight: 'plain',
+    moscowTime: 'plain'
+  },
+  placeholders: {
+    blockHeight: '{{blockheight}}',
+    moscowTime: '{{moscowtime}}',
+    moscowTimeAtBlockHeight: '{{moscowtime@blockheight}}'
+  }
 }
 
 export class BbsSettingTab extends PluginSettingTab {
-	plugin: BbsPlugin;
+  plugin: BbsPlugin;
 
-	constructor(app: App, plugin: BbsPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: BbsPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		const {containerEl} = this;
+  display(): void {
+    const {containerEl} = this;
 
-		containerEl.empty();
+    containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Block explorer')
-			.setDesc('Set block explorer for block height links')
-			.addDropdown(dropdown => dropdown
+    new Setting(containerEl)
+      .setName('Block explorer')
+      .setDesc('Set block explorer for block height links')
+      .addDropdown(dropdown => dropdown
         .addOption('', 'None')
-				.addOption('mempool-space', 'Mempool.space')
-				.addOption('blockstream-info', 'Blockstream.info')
-				.addOption('timechaincalendar-com', 'TimechainCalendar.com')
-				.setValue(this.plugin.settings.blockExplorer)
-				.onChange(async value => {
-					this.plugin.settings.blockExplorer = value as BlockExplorer;
-					await this.plugin.saveSettings()
-				})
-			)
-		
-		new Setting(containerEl).setName('Formats').setHeading();
+        .addOption('mempool-space', 'Mempool.space')
+        .addOption('blockstream-info', 'Blockstream.info')
+        .addOption('timechaincalendar-com', 'TimechainCalendar.com')
+        .setValue(this.plugin.settings.blockExplorer)
+        .onChange(async value => {
+          this.plugin.settings.blockExplorer = value as BlockExplorer;
+          await this.plugin.saveSettings()
+        })
+      )
+    
+    new Setting(containerEl).setName('Formats').setHeading();
 
-		new Setting(containerEl)
-			.setName('Block height format')
-			.setDesc('Set thousands separator')
-			.addDropdown(dropdown => dropdown
-				.addOption('plain', 'Plain (840000)')
-				.addOption('comma', 'Comma (840,000)')
-				.addOption('period', 'Period (840.000)')
-				.addOption('space', 'Space (840 000)')
-				.addOption('apostrophe', 'Apostrophe (840\'000)')
-				.addOption('underscore', 'Underscore (840_000)')
-				.setValue(this.plugin.settings.formats.blockHeight)
-				.onChange(async value => {
-					this.plugin.settings.formats.blockHeight = value as BlockHeightFormat;
-					await this.plugin.saveSettings();
-				})
-			)
-		
-		new Setting(containerEl)
-			.setName('Moscow time format')
-			.setDesc('Set time format separator')
-			.addDropdown(dropdown => dropdown
-				.addOption('plain', 'Plain (1566)')
-				.addOption('colon', 'Colon (15:66)')
-				.addOption('period', 'Period (15.66)')
-				.setValue(this.plugin.settings.formats.moscowTime)
-				.onChange(async value => {
-					this.plugin.settings.formats.moscowTime = value as MoscowTimeFormat;
-					await this.plugin.saveSettings();
-				})
-			)
-		
-		new Setting(containerEl).setName('Stamp Placeholders').setHeading()
-			.setDesc('');
-		new Setting(containerEl)
-			.setName('Block height placeholder')
-			.setDesc('')
-			.addText(text => text
-				.setPlaceholder('{{blockheight}}')
-				.setValue(this.plugin.settings.placeholders.blockHeight)
-				.onChange(async blockHeightPlaceholder => {
-					this.plugin.settings.placeholders.blockHeight = blockHeightPlaceholder;
-					await this.plugin.saveSettings();
-				})
-			)
-		new Setting(containerEl)
-			.setName('Moscow time placeholder')
-			.setDesc('')
-			.addText(text => text
-				.setPlaceholder('{{moscowtime}}')
-				.setValue(this.plugin.settings.placeholders.moscowTime)
-				.onChange(async moscowTimePlaceholder => {
-					this.plugin.settings.placeholders.moscowTime = moscowTimePlaceholder;
-					await this.plugin.saveSettings();
-				})
-			)
-		new Setting(containerEl)
-			.setName('Moscow time @ block height placeholder')
-			.setDesc('')
-			.addText(text => text
-				.setPlaceholder('{{moscowtime@blockheight}}')
-				.setValue(this.plugin.settings.placeholders.moscowTimeAtBlockHeight)
-				.onChange(async moscowTimeAtBlockHeightPlaceholder => {
-					this.plugin.settings.placeholders.moscowTimeAtBlockHeight = moscowTimeAtBlockHeightPlaceholder;
-					await this.plugin.saveSettings();
-				})
-			)
-	}
+    new Setting(containerEl)
+      .setName('Block height format')
+      .setDesc('Set thousands separator')
+      .addDropdown(dropdown => dropdown
+        .addOption('plain', 'Plain (840000)')
+        .addOption('comma', 'Comma (840,000)')
+        .addOption('period', 'Period (840.000)')
+        .addOption('space', 'Space (840 000)')
+        .addOption('apostrophe', 'Apostrophe (840\'000)')
+        .addOption('underscore', 'Underscore (840_000)')
+        .setValue(this.plugin.settings.formats.blockHeight)
+        .onChange(async value => {
+          this.plugin.settings.formats.blockHeight = value as BlockHeightFormat;
+          await this.plugin.saveSettings();
+        })
+      )
+    
+    new Setting(containerEl)
+      .setName('Moscow time format')
+      .setDesc('Set time format separator')
+      .addDropdown(dropdown => dropdown
+        .addOption('plain', 'Plain (1566)')
+        .addOption('colon', 'Colon (15:66)')
+        .addOption('period', 'Period (15.66)')
+        .setValue(this.plugin.settings.formats.moscowTime)
+        .onChange(async value => {
+          this.plugin.settings.formats.moscowTime = value as MoscowTimeFormat;
+          await this.plugin.saveSettings();
+        })
+      )
+    
+    new Setting(containerEl).setName('Stamp Placeholders').setHeading()
+      .setDesc('');
+    new Setting(containerEl)
+      .setName('Block height placeholder')
+      .setDesc('')
+      .addText(text => text
+        .setPlaceholder('{{blockheight}}')
+        .setValue(this.plugin.settings.placeholders.blockHeight)
+        .onChange(async blockHeightPlaceholder => {
+          this.plugin.settings.placeholders.blockHeight = blockHeightPlaceholder;
+          await this.plugin.saveSettings();
+        })
+      )
+    new Setting(containerEl)
+      .setName('Moscow time placeholder')
+      .setDesc('')
+      .addText(text => text
+        .setPlaceholder('{{moscowtime}}')
+        .setValue(this.plugin.settings.placeholders.moscowTime)
+        .onChange(async moscowTimePlaceholder => {
+          this.plugin.settings.placeholders.moscowTime = moscowTimePlaceholder;
+          await this.plugin.saveSettings();
+        })
+      )
+    new Setting(containerEl)
+      .setName('Moscow time @ block height placeholder')
+      .setDesc('')
+      .addText(text => text
+        .setPlaceholder('{{moscowtime@blockheight}}')
+        .setValue(this.plugin.settings.placeholders.moscowTimeAtBlockHeight)
+        .onChange(async moscowTimeAtBlockHeightPlaceholder => {
+          this.plugin.settings.placeholders.moscowTimeAtBlockHeight = moscowTimeAtBlockHeightPlaceholder;
+          await this.plugin.saveSettings();
+        })
+      )
+  }
 }
